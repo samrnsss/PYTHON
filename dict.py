79d7487840bd2829059c8a128eacc0ee51600989
Samreen Bano <samreen22g@gmail.com>
2026-09-25 23:42:23 +0530

    dictionary and its methods

--- dict.py
+++ dict.py
@@ -11,3 +11,13 @@ print(info)  # Output: {'name': 'aashna', 'cgpa': 9.0, 'age': 20, 'subject': ['m
 print(info["name"])  # Output: aashna
 print(info["cgpa"])  # Output: 9.0
 print(info[3.14])
+
+dict_keys = list(info.keys())
+print(dict_keys)  # Output: dict_keys(['name', 'cgpa', 'age', 'subject', 3.14])
+print(type(dict_keys))  # Output: <class 'list'>
+print(info.values())  # Output: dict_values(['aashna', 9.0, 20, ['maths', 'science', 'english'], 'PI'])
+print(info.values())  # Output: ['aashna', 9.0, 20, ['maths', 'science', 'english'], 'PI']
+print(info.items())  # Output: dict_items([('name', 'aashna'), ('cgpa', 9.0), ('age', 20), ('subject', ['maths', 'science', 'english']), (3.14, 'PI')])
+print(info.get("name"))  # Output: aashna
+print(info.update({"name": "Aashna", "age": 21}))  # Updates the name and age
+print(info)  # Output: {'name': 'Aashna', 'cgpa': 9.0, 'age': 21, 'subject': ['maths', 'science', 'english'], 3.14: 'PI'}
